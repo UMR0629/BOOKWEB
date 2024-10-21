bbf1f869a2796a317a7e793b613c9bccf0352c54
susumiya123 <2335915224@qq.com>
2024-10-21 16:08:14 +0800

    setting

--- BookWeb/BookWeb/BookWeb/settings.py
+++ BookWeb/BookWeb/BookWeb/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,7 +118,24 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+STATICFILES_DIRS = [
+    # 全局目录下的静态文件
+    BASE_DIR / "static",
+]
+PROFILE_ROOT = os.path.join(BASE_DIR, 'static/images/')
+RESUME_ROOT = os.path.join(BASE_DIR, 'resumes/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+try:
+    from .local_settings import *
+except ImportError:  # 捕获导入异常
+    pass
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
